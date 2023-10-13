3dca3e588423eea192e20da1b92af5afb32121df
mandi628 <amanda.aumann@gmail.com>
2023-10-13 11:52:07 -0400

    Update after JS Fundamentals 2 lesson

--- scripts/js-sample.js
+++ scripts/js-sample.js
@@ -1,2 +1,10 @@
-let message = "Good morning!";
-alert(message);
+// let message = "Good morning!";
+// alert(message);
+
+// let name = "Ilya";
+// alert( `hello ${1}`);
+// alert( `hello ${"name"}`);
+// alert( `hello ${name}`);
+
+const string = "The revolution will not be televised.";
+console.log(string);
