a929cef29a2efec26343b45799b6c352fbc7b321
mandi628 <amanda.aumann@gmail.com>
2024-02-05 12:12:30 -0500

    Added scripts

--- scripts/js-sample.js
+++ scripts/js-sample.js
@@ -6,5 +6,23 @@
 // alert( `hello ${"name"}`);
 // alert( `hello ${name}`);
 
-const string = "The revolution will not be televised.";
+/* const string = "The revolution will not be televised.";
 console.log(string);
+
+const myText = "I am a string.";
+const newString = myText.replace("string", "garlic sausage");
+console.log(myText);
+console.log("Oops...");
+console.log(newString); */
+
+// const myArray = ["I", "love", "chocolate", "frogs"];
+// const madeAString = myArray.join(" ");
+// console.log(madeAString);
+// the join() function takes an array, joins
+// all the array items together into a single
+// string, and returns this new string
+
+const myNumber = Math.random();
+console.log(myNumber);
+// the random() function generates a random number between
+// 0 and up to but not including 1, and returns that number
